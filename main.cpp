b60d02f50a9ca831ecb78caca0b8ae8147b2ac3b
Joe B <74583686+Fizz14@users.noreply.github.com>
2023-11-05 14:20:53 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -218,8 +218,6 @@ void messagePassingCheck(int NUM_VALS, float *values, int num_procs)
   // Scatter the array among processors
   MPI_Scatter(values, local_size, MPI_FLOAT, local_values, local_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
   
-  //build a binary tree?
-  
   //First, I want to do some testing with communicating between processes
   //Let's get a process to send another process a message
 
@@ -285,8 +283,9 @@ void messagePassingCheck(int NUM_VALS, float *values, int num_procs)
 struct node
 {
   int value;
-  struct node *left;
-  struct node *right;
+  struct node *left = nullptr;
+  struct node *right = nullptr;
+  struct node *parent = nullptr;
 };
 
 void printTree(std::string prefix, node* n, bool isLeft) {
@@ -298,7 +297,7 @@ void printTree(std::string prefix, node* n, bool isLeft) {
     } else {
       printf("└──");
     }
-    printf("%.6f\n",n->value);
+    printf("%d\n",n->value,n);
     if(isLeft) {
       printTree(prefix + "|   ", n->left, true);
       printTree(prefix + "|   ", n->right, false);
@@ -311,11 +310,13 @@ void printTree(std::string prefix, node* n, bool isLeft) {
 
 void populateNode(node* a, int height, int maxheight, vector<int>* nodebank)
 {
-  printf("populateNode %d/%d\n", height, maxheight);
+  if(height == maxheight) {return;}
+  //printf("populateNode %d/%d\n", height, maxheight);
   //left node has the same value
-  printf("Made a left node\n");
+  //printf("Made a left node\n");
   node* left = new node;
   left->value = a->value;
+  left->parent = a;
   a->left = left;
   if(height < maxheight) {
     populateNode(left, height+1, maxheight, nodebank);
@@ -323,9 +324,10 @@ void populateNode(node* a, int height, int maxheight, vector<int>* nodebank)
 
   node* right = new node;
   right->value = nodebank->back();
+  right->parent = a;
   nodebank->pop_back();
-  printf("Popped a node from the nodebank for a right node!\n");
-  printf("That node was %d\n", right->value);
+//  printf("Popped a node from the nodebank for a right node!\n");
+//  printf("That node was %d\n", right->value);
   a->right = right;
 
   if(height < maxheight) {
@@ -333,31 +335,144 @@ void populateNode(node* a, int height, int maxheight, vector<int>* nodebank)
   }
 }
 
+node* searchForLeaf(node* n, int value) {
+  if(n == nullptr) {return nullptr;}
+  if(n->left != nullptr) {
+    node* leftSearchResult = searchForLeaf(n->left, value);
+    node* rightSearchResult = searchForLeaf(n->right, value);
+    if(leftSearchResult != nullptr) {
+      return leftSearchResult;
+    } else {
+      return rightSearchResult;
+    }
+  } else {
+    if(n->value == value) {
+      return n;
+    }
+  }
+  return nullptr;
+}
+
+string stepUpTree(node* n, string ret) {
+  
+  ret += to_string(n->value);
+  if(n->parent == nullptr) { 
+    return ret; 
+  } else {
+    return stepUpTree(n->parent, ret);
+  }
+
+}
+
 void parallelMergesort(int NUM_VALS, float *values, int num_procs)
 { 
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+  // Calculate local size based on rank and array size
+  int local_size = NUM_VALS / num_procs;
+  int start = rank * local_size;
+  int end = (rank == num_procs - 1) ? NUM_VALS : start + local_size;
+
+  //local_size = end - start;
+
+  float* local_values = (float*)malloc(local_size * sizeof(float));
+
+  // Scatter the array among processors
+  MPI_Scatter(values, local_size, MPI_FLOAT, local_values, local_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(rank == 1) {
+    printf("1: Here are my values.\n");
+    for(int i = 0; i < local_size; i++) {
+      printf("1: Value: %.6f\n", local_values[i]);
+    }
+  }
+
   //generate a binary tree with num_procs leaves
   //it's height is equal to log_2(num_procs)
+  node* root = new node;
+  root->value = 0;
+  int treesHeight = log2(num_procs) + 1;
+  //printf("building a binary tree\n", treesHeight);
+  
+  vector<int> *nodebank = new vector<int>(num_procs); //a vector of integers of nodes which we can add
+  for(int i = 1; i < num_procs; i++) {
+    nodebank->push_back(i);
+  }
+
+  populateNode(root, 1, treesHeight, nodebank);
+
+  //check tree
   if(rank == 0) {
-    node* root = new node;
-    root->value = 0;
-    int treesHeight = log2(num_procs) + 1;
-    printf("building a binary tree\n", treesHeight);
-    
-    vector<int> *nodebank = new vector<int>(num_procs); //a vector of integers of nodes which we can add
-    for(int i = 1; i < num_procs; i++) {
-      printf("Does this line crash?\n");
-      nodebank->push_back(i);
-      printf("Nodebank has %d\n",i);
-    }
-
-    populateNode(root, 1, treesHeight, nodebank);
-
-    //check tree
     printTree("", root, false);
   }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  //the tree is a representation of how the threads will pass data.
+  
+  //search tree for leaf with value == rank
+  node* myLeaf = searchForLeaf(root, rank);
+
+  //derive a list of destinations that this thread's data will go to
+  string destList = stepUpTree(myLeaf, "");
+  printf("My rank is %d and my dest list is %s.\n", rank, destList.c_str());
+  
+  destList.erase(0,1);
+
+  int i = 1; 
+  bool work = 1; //some processors pass through the loop and don't work
+  int current_size = local_size;
+  for(;;) {
+    
+    if(work) {
+      mergesort(local_values, 0, current_size-1);
+  
+      string thisDest = "";
+      thisDest += destList[0];
+      destList.erase(0,1);
+      int dest = stoi(thisDest);
+  
+      if(dest == rank) {
+        //we need to listen for a message from the other processor
+        float* foreign_values = (float*)malloc(current_size * sizeof(float));
+        MPI_Recv(foreign_values, current_size, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        printf("0: Here are his values.\n");
+        for(int i = 0; i < local_size; i++) {
+          printf("0: Value: %.6f\n", foreign_values[i]);
+        }
+
+        float* combined_values = new float[current_size * 2];
+        std::copy(local_values, local_values + local_size, combined_values);
+        std::copy(foreign_values, foreign_values + local_size, combined_values + local_size);
+        merge(combined_values, 0, current_size-1, (current_size * 2)-1);
+
+        free(local_values);
+        free(foreign_values);
+        local_values = combined_values;
+
+        printf("My rank is %d and I Received message from another processor, check the sorted resultant value of local_values\n", rank);
+
+        for(int i = 0; i < current_size * 2; i++) {
+          printf("%d: Value: %.6f\n", rank, local_values[i]);
+        }
+        
+      } else {
+        //send it to dest
+        MPI_Send(local_values, current_size, MPI_FLOAT, dest, 0, MPI_COMM_WORLD);
+        printf("My rank is %d and I'm sending to %d.\n", rank, dest);
+        work = 0; //this processor now rests
+      }
+    }
+
+    current_size = current_size * 2;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if(destList.size() == 0) {break;}
+  }
+ 
+  
 }
 
 
@@ -413,8 +528,8 @@ int main(int argc, char* argv[])
     parallel_array_fill(NUM_VALS, values, num_procs, rank);
 
     
-    //messagePassingCheck(NUM_VALS, values, num_procs);
-    parallelMergesort(NUM_VALS, values, num_procs);
+    messagePassingCheck(NUM_VALS, values, num_procs);
+    //parallelMergesort(NUM_VALS, values, num_procs);
 
 
     // Check if values is sorted
