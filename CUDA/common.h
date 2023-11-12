e75302d8cc9eac1d45f8ef6b79037db253d7f182
Fredy H Medawar <fredymedawar@login4.cluster>
2023-11-12 11:55:26 -0600

    Updated CMake build, Added instrumentation to CUDA main, and added 1% perturbed input generation to CUDA

--- CUDA/common.h
+++ CUDA/common.h
@@ -2,15 +2,16 @@
 #include <stdio.h>
 #include <time.h>
 
-//#include <caliper/cali.h>
-//#include <caliper/cali-manager.h>
-//#include <adiak.hpp>
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
 
 #define bitonic_sort_step_region "bitonic_sort_step"
 #define selection_sort_region "selection_sort_region"
 #define cudaMemcpy_host_to_device "cudaMemcpy_host_to_device"
 #define cudaMemcpy_device_to_host "cudaMemcpy_device_to_host"
 #define array_fill_name "array_fill"
+#define sort_check_name "sort_check"
 
 void bitonic_sort(float *values, float* dev_values, int NUM_VALS, int THREADS, int BLOCKS);
 void quicksort(float *values, float* dev_values, int NUM_VALS, int THREADS, int BLOCKS);
--- CUDA/main.cu
+++ CUDA/main.cu
@@ -37,6 +37,18 @@ __global__ void device_array_fill(float *arr, int length, unsigned long long see
         {
             arr[i] = (float)(length - i);
         }
+        else if (array_fill_type == 3)
+        {
+            int val = (int)(curand_uniform(&state) * 100) + 1;
+            if (val > 1)
+            {
+                arr[i] = (float)i;
+            }
+            else
+            {
+                arr[i] = curand_uniform(&state);
+            }
+        }
     }
 }
 
@@ -61,6 +73,10 @@ int main(int argc, char *argv[])
 
     int BLOCKS = NUM_VALS / THREADS;
 
+    // Create caliper ConfigManager object
+    cali::ConfigManager mgr;
+    mgr.start();
+
     printf("Number of threads: %d\n", THREADS);
     printf("Number of values: %d\n", NUM_VALS);
     printf("Number of blocks: %d\n", BLOCKS);
@@ -79,47 +95,50 @@ int main(int argc, char *argv[])
     //MEM COPY FROM HOST TO DEVICE
 
     // Start host to device event
-    //CALI_MARK_BEGIN(cudaMemcpy_host_to_device);
+    CALI_MARK_BEGIN(cudaMemcpy_host_to_device);
 
     cudaMemcpy(dev_values, values, size, cudaMemcpyHostToDevice);
     cudaMemcpy(dev_sorted, &sorted, sizeof(int), cudaMemcpyHostToDevice);
 
     // End  host to device Event
     cudaDeviceSynchronize();
-    //CALI_MARK_END(cudaMemcpy_host_to_device);
+    CALI_MARK_END(cudaMemcpy_host_to_device);
 
     // Start Array Fill Event
-    //CALI_MARK_BEGIN(array_fill_name);
+    CALI_MARK_BEGIN(array_fill_name);
     srand(time(NULL));
     unsigned long long seed = 10; // (unsigned long long)rand()
     device_array_fill<<<BLOCKS, THREADS>>>(dev_values, NUM_VALS, seed, array_fill_type);
 
     // End Array Fill Event
     cudaDeviceSynchronize();
-    //CALI_MARK_END(array_fill_name);
+    CALI_MARK_END(array_fill_name);
 
     if (sort_alg == 0)
     {
         bitonic_sort(values, dev_values, NUM_VALS, THREADS, BLOCKS);
     } else if(sort_alg==1) {
-        oddeven_sort(values, dev_values, NUM_VALS, THREADS, BLOCKS);
+        // oddeven_sort(values, dev_values, NUM_VALS, THREADS, BLOCKS);
     } else if(sort_alg==2) {
         selectionsort(values, dev_values, NUM_VALS, THREADS, BLOCKS);
     }
 
+    CALI_MARK_BEGIN(sort_check_name);
     device_sort_check<<<BLOCKS, THREADS>>>(dev_values, NUM_VALS, dev_sorted);
+    cudaDeviceSynchronize();
+    CALI_MARK_END(sort_check_name);
 
     //MEM COPY FROM DEVICE TO HOST
 
     // Start device to host event
-    //CALI_MARK_BEGIN(cudaMemcpy_device_to_host);
+    CALI_MARK_BEGIN(cudaMemcpy_device_to_host);
 
     cudaMemcpy(values, dev_values, size, cudaMemcpyDeviceToHost);
     cudaMemcpy(&sorted, dev_sorted, sizeof(int), cudaMemcpyDeviceToHost);
 
     // End device to host Event
     cudaDeviceSynchronize();
-    //CALI_MARK_END(cudaMemcpy_device_to_host);
+    CALI_MARK_END(cudaMemcpy_device_to_host);
 
     cudaFree(dev_values);
 
@@ -132,5 +151,29 @@ int main(int argc, char *argv[])
         printf("Array is not sorted\n");
     }
 
+    adiak::init(NULL);
+    adiak::user();
+    adiak::launchdate();
+    adiak::libraries();
+    adiak::cmdline();
+    adiak::clustername();
+    adiak::value("num_threads", THREADS);
+    adiak::value("num_blocks", BLOCKS);
+    adiak::value("num_vals", NUM_VALS);
+    adiak::value("datatype_size", sizeof(float));
+
+    if (array_fill_type == 0) adiak::value("array_fill_type", "random");
+    else if (array_fill_type == 1) adiak::value("array_fill_type", "sorted");
+    else if (array_fill_type == 2) adiak::value("array_fill_type", "reverse_sorted");
+    else if (array_fill_type == 3) adiak::value("array_fill_type", "sorted_1%_perturbed");
+
+    if (sort_alg == 0) adiak::value("sort_alg", "bitonic_sort");
+    else if (sort_alg == 1) adiak::value("sort_alg", "oddeven_sort");
+    else if (sort_alg == 2) adiak::value("sort_alg", "selection_sort");
+
     array_print(values, NUM_VALS);
+
+    // Flush Caliper output before finalizing MPI
+    mgr.stop();
+    mgr.flush();
 }
